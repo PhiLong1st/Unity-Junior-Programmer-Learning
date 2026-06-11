8e8cd9887f566f38445391d576890d13e5a07552
PhiLong1st <philongnguyen1704@gmail.com>
2026-06-11 10:25:57 +0700

    feat: finish mod the cube challenge

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+
+    void Start()
+    {
+        Material material = Renderer.material;
+        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+    }
+
+    void Update()
+    {
+        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.localScale = Vector3.one * 0.3f * Mathf.Abs(2 * Mathf.Sin(Time.time));
+    }
+}

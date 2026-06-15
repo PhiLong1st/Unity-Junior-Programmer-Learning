5582f102803bc011808dedc9eb33d9c4b94eaf2a
PhiLong1st <philongnguyen1704@gmail.com>
2026-06-16 02:06:38 +0700

    feat: finish prototype 5

--- Assets/Course Library Prototype 5/Scripts/GameManager.cs
+++ Assets/Course Library Prototype 5/Scripts/GameManager.cs
@@ -7,48 +7,48 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public List<GameObject> targets;
-    public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI gameOverText;
-    public bool isGameActive;
-    public Button restartButton;
-    private int score;
+  public List<GameObject> targets;
+  public TextMeshProUGUI scoreText;
+  public TextMeshProUGUI gameOverText;
+  public bool isGameActive;
+  public Button restartButton;
+  private int score;
 
-    private readonly float spawnRate = 1.0f;
+  private readonly float spawnRate = 1.0f;
 
-    private void Start()
+  private void Start()
+  {
+    isGameActive = true;
+    StartCoroutine(SpawnTarget());
+    score = 0;
+    UpdateScore(0);
+  }
+
+  private IEnumerator SpawnTarget()
+  {
+    while (isGameActive)
     {
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
+      yield return new WaitForSeconds(spawnRate);
+      int index = Random.Range(0, targets.Count);
+      Instantiate(targets[index]);
     }
+  }
 
-    private IEnumerator SpawnTarget()
-    {
-        while (isGameActive)
-        {
-            yield return new WaitForSeconds(spawnRate);
-            int index = Random.Range(0, targets.Count);
-            Instantiate(targets[index]);
-        }
-    }
+  public void UpdateScore(int scoreToAdd)
+  {
+    score += scoreToAdd;
+    scoreText.text = "Score: " + score;
+  }
 
-    public void UpdateScore(int scoreToAdd)
-    {
-        score += scoreToAdd;
-        scoreText.text = "Score: " + score;
-    }
+  public void GameOver()
+  {
+    gameOverText.gameObject.SetActive(true);
+    isGameActive = false;
+    restartButton.gameObject.SetActive(true);
+  }
 
-    public void GameOver()
-    {
-        gameOverText.gameObject.SetActive(true);
-        isGameActive = false;
-        restartButton.gameObject.SetActive(true);
-    }
-
-    public void RestartGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+  public void RestartGame()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  }
 }
